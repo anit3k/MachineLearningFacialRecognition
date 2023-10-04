4c5763a68f3d39499c27c358af041b6c0eaf68a6
anit3k <steffen.halberg@gmail.com>
2023-10-04 16:57:04 +0200

    refactor of home controller

--- MachineLearningFacialRecognition.Web/Controllers/HomeController.cs
+++ MachineLearningFacialRecognition.Web/Controllers/HomeController.cs
@@ -1,19 +1,26 @@
 ﻿using MachineLearningFacialRecognition.FaceRegService;
+using MachineLearningFacialRecognition.FaceRegService.Models;
 using MachineLearningFacialRecognition.FileHandler;
 using MachineLearningFacialRecognition.Web.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.ML.Data;
 using System.Diagnostics;
 
 namespace MachineLearningFacialRecognition.Web.Controllers
 {
+    /// <summary>
+    /// This class is used for the website as the only controller needed to handle
+    /// the prediction and training of the ml model
+    /// </summary>
     public class HomeController : Controller
     {
+        #region fields
         private readonly ILogger<HomeController> _logger;
         private readonly IPredictor _predictor;
         private readonly IFileHandlerService _fileHandler;
         private readonly ITrainer _trainer;
+        #endregion
 
+        #region Constructor
         public HomeController(ILogger<HomeController> logger, IPredictor predictor, IFileHandlerService fileHandler, ITrainer trainer)
         {
             _logger = logger;
@@ -21,7 +28,9 @@ namespace MachineLearningFacialRecognition.Web.Controllers
             _fileHandler = fileHandler;
             _trainer = trainer;
         }
+        #endregion
 
+        #region Prediction Page
         [HttpGet]
         public IActionResult Index()
         {
@@ -31,38 +40,12 @@ namespace MachineLearningFacialRecognition.Web.Controllers
         [HttpPost]
         public IActionResult Index(ImageUploadViewModel model)
         {
-            var temp = ConvertIFormFileToBase64(model.Image);
+            var result = PredictImage(model);            
+            return View(AddResultToViewModel(model, result));
+        }       
+        #endregion
 
-            var imageToPredict = _fileHandler.SaveFile(temp);
-
-            var result = _predictor.ClassifySingleImage(imageToPredict);
-
-            model.PredictionLabel = result.PredictedLabelValue;
-
-            model.PredictionPercentage = result.Score.Max();
-
-            _fileHandler.DeleteImage(imageToPredict);
-
-            return View(model);
-        }
-
-        private string ConvertIFormFileToBase64(IFormFile file)
-        {
-            using (MemoryStream memoryStream = new MemoryStream())
-            {
-                // Copy the file content to memory stream
-                file.CopyTo(memoryStream);
-
-                // Get the byte array from memory stream
-                byte[] bytes = memoryStream.ToArray();
-
-                // Convert byte array to Base64 string
-                string base64String = Convert.ToBase64String(bytes);
-
-                return base64String;
-            }
-        }
-
+        #region Training Page
         [HttpGet]
         public IActionResult Trainer()
         {
@@ -73,19 +56,8 @@ namespace MachineLearningFacialRecognition.Web.Controllers
         public IActionResult Trainer(FileUploadViewModel model)
         {
             try
-            {
-                List<string> files = new List<string>();
-
-                foreach (var file in model.Files)
-                {
-                    var temp = ConvertIFormFileToBase64(file);
-                    files.Add(temp);
-                }
-                foreach (var item in files)
-                {
-                    var path = _fileHandler.SaveFile(item);
-                    _fileHandler.AddToTsvFile(Path.GetFileName(path), model.Tag);
-                }
+            {                
+                AddImagesToTrainerModel(model);
                 _trainer.TrainModel();
                 model.StatusMessage = "Success!";
             }
@@ -96,12 +68,65 @@ namespace MachineLearningFacialRecognition.Web.Controllers
                 throw;
             }
             return View(model);
-        }
+        }        
+        #endregion
 
+        #region Error page
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        #endregion
+
+        #region private methods
+        private ImagePrediction PredictImage(ImageUploadViewModel model)
+        {
+            var base64string = ConvertIFormFileToBase64String(model.Image);
+            var imageToPredict = _fileHandler.SaveFile(base64string);
+            var result = _predictor.ClassifySingleImage(imageToPredict);
+            _fileHandler.DeleteImage(imageToPredict);
+            return result;
+        }
+
+        private ImageUploadViewModel AddResultToViewModel(ImageUploadViewModel model, ImagePrediction result)
+        {
+            model.PredictionLabel = result.PredictedLabelValue;
+            model.PredictionPercentage = result.Score.Max();
+            return model;
+        }
+
+        private void AddImagesToTrainerModel(FileUploadViewModel model)
+        {
+            List<string> files = GetBase64StringFromImages(model);
+            foreach (var item in files)
+            {
+                var path = _fileHandler.SaveFile(item);
+                _fileHandler.AddToTsvFile(Path.GetFileName(path), model.Tag);
+            }
+        }
+
+        private List<string> GetBase64StringFromImages(FileUploadViewModel model)
+        {
+            List<string> files = new List<string>();
+            foreach (var file in model.Files)
+            {
+                var temp = ConvertIFormFileToBase64String(file);
+                files.Add(temp);
+            }
+            return files;
+        }
+
+        private string ConvertIFormFileToBase64String(IFormFile file)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                file.CopyTo(memoryStream);
+                byte[] bytes = memoryStream.ToArray();
+                string base64String = Convert.ToBase64String(bytes);
+                return base64String;
+            }
+        }
+        #endregion
     }
 }
